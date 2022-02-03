2ba29254aeb4e5832e029584775cb7c995cbc1d1
Saikiran <saikiranmohanty@gmail.com>
2022-02-03 19:56:43 +0530

    Updated Backend

--- Products/script.js
+++ Products/script.js
@@ -5,7 +5,7 @@ contact Us - hellosai090@gmail.com */
 // -------------Whatsapp Integration--------------
 function textencode(title){
     let str="Hello, I would like to buy "+title+" from you";
-    let url="https://wa.me/8249063374?text=";//Your Business Whatsapp Number Here
+    let url="https://wa.me/918249063374?text=";//Your Business Whatsapp Number Here
     let encoded =url + encodeURIComponent(str);
     return encoded;
 }
